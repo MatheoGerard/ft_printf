1044311fcade1fa24680e7e8690f3e19525ea167
Matheo Gerard <mgerard@fu-r4-p6.s19.be>
2026-04-18 10:11:13 +0200

    add char format in my printf function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mgerard <mgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 08:30:08 by mgerard           #+#    #+#             */
-/*   Updated: 2026/04/18 09:56:24 by mgerard          ###   ########.fr       */
+/*   Updated: 2026/04/18 10:10:16 by mgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int ft_printf(const char *str, ...)
 				ft_putstr(va_arg(args, char *));
 			else if (str[i] == 'd')
 				ft_putnb(va_arg(args, int));
+			else if (str[i] == 'c')
+				ft_putchar(va_arg(args, int));
 			i++;
 		}
 		ft_putchar(str[i]);
@@ -43,5 +45,6 @@ int main(void)
 
 	const char *str = "BONJOUR";
 	int nb = 42;
-	ft_printf("j ai envie de te dire %s au moins %d fois", str, nb);
+	char dot = '.';
+	ft_printf("j ai envie de te dire %s au moins %d fois %c", str, nb, dot);
 }
