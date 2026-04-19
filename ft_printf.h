2c8c4ba9597c9ecc6fe9d6e9cee3936aa93c2bf8
Matheo Gerard <mgerard@fu-r4-p6.s19.be>
2026-04-19 16:07:15 +0200

    ft_printf.h normed

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,20 @@
 /*   By: mgerard <mgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 08:40:08 by mgerard           #+#    #+#             */
-/*   Updated: 2026/04/19 14:26:33 by mgerard          ###   ########.fr       */
+/*   Updated: 2026/04/19 16:06:16 by mgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdint.h>
-#include <stdlib.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-int	ft_putchar(char c);
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdint.h>
+# include <stdlib.h>
+
+int		ft_putchar(char c);
 void	ft_putnb(int nb);
 void	ft_dec_to_hex_ptr(uintptr_t nb, char *base, int *count);
 void	ft_putnb_unsigned(unsigned int nb);
@@ -26,3 +29,5 @@ char	*ft_itoa(int n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(char *str);
 char	*ft_itoa_unsigned(unsigned int n);
+
+#endif // FT_PRINTF_H
